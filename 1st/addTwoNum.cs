6c72891afad86b24e7aefeae9b31814f65d29590
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-02-27 01:25:41 +0600

    ok

---
+++ 1st/addTwoNum.cs
@@ -0,0 +1,11 @@
+using System;
+public class addTwoNum
+{
+    public static void Main(string[] args)
+    {
+        int a = 20;
+        int b = 30;
+        int c = a + b;
+        Console.WriteLine(c);
+    }
+}
