dca5f10ebcae5fa200b2ec9ab39bf7e2967bcda5
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-02-27 01:50:55 +0600

    ok

--- 1st/quadratic.cs
+++ 1st/quadratic.cs
@@ -5,23 +5,23 @@ public class Quadtest
     {
     int a,b,c,d;
     double x,x1,x2 ;
-    console.Writeline("enter the value of a");
-    a=convert.ToInt16(console.Readline( ));
-    console.Writeline("enter the value of b");
-    b=convert.ToInt16(console.Readline( ));
-    console.Writeline("enter the value of c");
-    c=convert.ToInt16(console.Readline( ));
+    Console.Writeline("enter the value of a");
+    a=Convert.ToInt16(Console.Readline( ));
+    Console.Writeline("enter the value of b");
+    b=Convert.ToInt16(Console.Readline( ));
+    Console.Writeline("enter the value of c");
+    c=Convert.ToInt16(Console.Readline( ));
     d=b*b-4*a*c;
     if(d>0)
     {
     x1=(-b+math.sqrt(d))/(2*a);
     x2=(-b-math.sqrt(d))/(2*a);
-    console.Writeline("x1="+x1+"x2="+x2);
+    Console.Writeline("x1="+x1+"x2="+x2);
     }
     else if(d==0)
     {
     x=-b/(2*a);
-    console.Writeline("x="+x);
+    Console.Writeline("x="+x);
     }
     else
     Console.Writeline("root are imaginary ");
