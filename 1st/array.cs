0dd818059b508bb9b9e57582daac8a1ec6362fe2
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-03-30 00:10:02 +0600

    ok

---
+++ 1st/array.cs
@@ -0,0 +1,11 @@
+using System;
+public class arrays
+{
+    public static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        foreach (var item in array) {
+            Console.WriteLine(item);
+        }
+    }
+}
