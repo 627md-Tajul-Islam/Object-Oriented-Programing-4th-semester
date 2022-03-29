7d087f5c3326ddbc5475244937ceac5a358fece5
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-03-29 22:48:25 +0600

    ok

---
+++ 1st/largest_among_three_Numbers.cs
@@ -0,0 +1,24 @@
+using System;
+namespace Number {
+   class number {
+      static void Main(string[] args) {
+         int num1, num2, num3;
+         num1 = 100; num2 = 20; num3 = 50;
+        if (num1 > num2) {
+            if (num1 > num3) {
+               Console.Write("Number one is the largest\n");
+            }
+      }
+        if (num2 > num1) {
+            if (num2 > num3) {
+               Console.Write("Number two is the largest\n");
+            }
+      }
+        if (num3 > num1) {
+            if (num3 > num2) {
+               Console.Write("Number three is the largest\n");
+            }
+      }
+      }
+   }
+}
